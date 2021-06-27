f0752a521c0e2a4ffae82cade999e9f12440abfe
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 22:03:30 +0530

    feat: Override create() in ConsumerFactory

--- src/main/java/io/odpf/kafkaclient/consumer/ConsumerFactory.java
+++ src/main/java/io/odpf/kafkaclient/consumer/ConsumerFactory.java
@@ -1,11 +1,14 @@
 package io.odpf.kafkaclient.consumer;
 
 import io.odpf.kafkaclient.ClientFactory;
+import io.odpf.kafkaclient.PropertiesNotSetException;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class ConsumerFactory extends ClientFactory {
 
     @Override
     public Consumer create() {
-        return null;
+        if (getProperties() == null) throw new PropertiesNotSetException();
+        return new Consumer(new KafkaConsumer<>(getProperties()));
     }
 }
