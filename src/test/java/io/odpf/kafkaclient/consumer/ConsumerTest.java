45299e45fe7c133fdaf06385dcb3a85df63cd613
sumitaich1998 <aich.1998@gmail.com>
2021-06-28 00:14:35 +0530

    test: Add setUp() to initialize consumer object

--- src/test/java/io/odpf/kafkaclient/consumer/ConsumerTest.java
+++ src/test/java/io/odpf/kafkaclient/consumer/ConsumerTest.java
@@ -1,4 +1,41 @@
 package io.odpf.kafkaclient.consumer;
 
+import org.junit.Before;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ConsumerTest {
+    File propertiesFile;
+    Consumer consumer;
+
+    @Before
+    public void setUp() {
+        propertiesFile = new File("consumer_temp.properties");
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(propertiesFile));
+
+            bufferedWriter.write("bootstrap.servers localhost:9092\n");
+            bufferedWriter.write("group.id test\n");
+            bufferedWriter.write("enable.auto.commit true\n");
+            bufferedWriter.write("auto.commit.interval.ms 1000\n");
+            bufferedWriter.write("session.timeout.ms 30000\n");
+            bufferedWriter.write("key.deserializer org.apache.kafka.common.serialization.StringDeserializer\n");
+            bufferedWriter.write("value.deserializer org.apache.kafka.common.serialization.StringDeserializer");
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ConsumerFactory consumerFactory = new ConsumerFactory();
+
+        try {
+            consumerFactory.configure(propertiesFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        consumer = consumerFactory.create();
+    }
 }
