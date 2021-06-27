dabd12a10e26c9fcdadc8654541bdcb1866808f4
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 21:58:40 +0530

    feat: Add Consumer constructor

--- src/main/java/io/odpf/kafkaclient/consumer/Consumer.java
+++ src/main/java/io/odpf/kafkaclient/consumer/Consumer.java
@@ -1,10 +1,23 @@
 package io.odpf.kafkaclient.consumer;
 
 import io.odpf.kafkaclient.Client;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Consumer extends Client {
+
+    private final KafkaConsumer<String, String> kafkaConsumer;
+    private final BufferedReader bufferedReader;
+    private String topic;
+
+    public Consumer(KafkaConsumer<String, String> kafkaConsumer) {
+        this.kafkaConsumer = kafkaConsumer;
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
     @Override
     public void interact() throws IOException {
 
