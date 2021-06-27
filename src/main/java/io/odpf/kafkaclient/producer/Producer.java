b21818338a76f7c18aa720106a6e930c5cca82f6
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 22:00:08 +0530

    feat: Add Producer constructor

--- src/main/java/io/odpf/kafkaclient/producer/Producer.java
+++ src/main/java/io/odpf/kafkaclient/producer/Producer.java
@@ -1,10 +1,22 @@
 package io.odpf.kafkaclient.producer;
 
 import io.odpf.kafkaclient.Client;
+import org.apache.kafka.clients.producer.KafkaProducer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Producer extends Client {
+
+    private final KafkaProducer<String, String> kafkaProducer;
+    private final BufferedReader bufferedReader;
+
+    public Producer(KafkaProducer<String, String> kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
     @Override
     public void interact() throws IOException {
 
