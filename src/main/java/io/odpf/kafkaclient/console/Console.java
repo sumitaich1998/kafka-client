e98ab8e2775344a36d790f8d7e141f48a1080455
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 20:42:59 +0530

    feat: Add main method

--- src/main/java/io/odpf/kafkaclient/console/Console.java
+++ src/main/java/io/odpf/kafkaclient/console/Console.java
@@ -1,7 +1,23 @@
 package io.odpf.kafkaclient.console;
 
-public class Console {
-    public static void main(String... args) {
+import io.odpf.kafkaclient.Client;
+import io.odpf.kafkaclient.ClientFactory;
+import io.odpf.kafkaclient.consumer.ConsumerFactory;
+import io.odpf.kafkaclient.producer.ProducerFactory;
 
+import java.io.File;
+import java.io.IOException;
+
+public class Console {
+
+    public static void main(String... args) throws IOException {
+        File propertiesFile = new File(args[1]);
+        ClientFactory clientFactory = (args[0].equals("producer")) ?
+                new ProducerFactory() : new ConsumerFactory();
+
+        clientFactory.configure(propertiesFile);
+        Client client = clientFactory.create();
+
+        client.interact();
     }
 }
