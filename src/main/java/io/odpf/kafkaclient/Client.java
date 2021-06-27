ae206b172a918ecf0905a0e982ec4a4a880d1813
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 19:44:52 +0530

    feat: Add Client constructor

--- src/main/java/io/odpf/kafkaclient/Client.java
+++ src/main/java/io/odpf/kafkaclient/Client.java
@@ -1,4 +1,19 @@
 package io.odpf.kafkaclient;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public abstract class Client {
+    private final Logger log;
+
+    public Client() {
+        log = Logger.getLogger("my.logger");
+        log.setLevel(Level.ALL);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        handler.setFormatter(new SimpleFormatter());
+        log.addHandler(handler);
+    }
 }
