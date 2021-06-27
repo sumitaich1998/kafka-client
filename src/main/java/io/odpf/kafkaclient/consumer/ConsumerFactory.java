49a5105f0a716ec124ac99fc73c15c4e88683cbd
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 20:25:36 +0530

    feat: Override create() in ConsumerFactory

--- src/main/java/io/odpf/kafkaclient/consumer/ConsumerFactory.java
+++ src/main/java/io/odpf/kafkaclient/consumer/ConsumerFactory.java
@@ -3,4 +3,9 @@ package io.odpf.kafkaclient.consumer;
 import io.odpf.kafkaclient.ClientFactory;
 
 public class ConsumerFactory extends ClientFactory {
+
+    @Override
+    public Consumer create() {
+        return null;
+    }
 }
