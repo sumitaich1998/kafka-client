f8763ad2036019245552d7a6667e7bc95c2f1210
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 20:00:19 +0530

    feat: Implement interact() in Consumer

--- src/main/java/io/odpf/kafkaclient/consumer/Consumer.java
+++ src/main/java/io/odpf/kafkaclient/consumer/Consumer.java
@@ -2,5 +2,11 @@ package io.odpf.kafkaclient.consumer;
 
 import io.odpf.kafkaclient.Client;
 
+import java.io.IOException;
+
 public class Consumer extends Client {
+    @Override
+    public void interact() throws IOException {
+
+    }
 }
