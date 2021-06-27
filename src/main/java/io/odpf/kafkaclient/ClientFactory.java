a88bee14bdddf795ab9e74122e1f154c929b6159
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 20:18:47 +0530

    feat: Implement configure() method to set properties

--- src/main/java/io/odpf/kafkaclient/ClientFactory.java
+++ src/main/java/io/odpf/kafkaclient/ClientFactory.java
@@ -1,4 +1,24 @@
 package io.odpf.kafkaclient;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public abstract class ClientFactory {
+    private Properties properties;
+
+    public final void configure(File file) throws IOException {
+        properties = new Properties();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String propertyText = "";
+        while ((propertyText = bufferedReader.readLine()) != null) {
+            String property = propertyText.split(" ")[0];
+            String value = propertyText.split(" ")[1];
+
+            properties.put(property, value);
+        }
+    }
 }
