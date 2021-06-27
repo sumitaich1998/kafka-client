efd045840c9662038c59d974373d0eb8f2c77319
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 20:48:24 +0530

    feat: Override printStackTrace()

--- src/main/java/io/odpf/kafkaclient/PropertiesNotSetException.java
+++ src/main/java/io/odpf/kafkaclient/PropertiesNotSetException.java
@@ -1,4 +1,21 @@
 package io.odpf.kafkaclient;
 
-public class PropertiesNotSetException extends RuntimeException{
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class PropertiesNotSetException extends RuntimeException {
+
+    @Override
+    public void printStackTrace() {
+        Logger log = Logger.getLogger("my.logger");
+        log.setLevel(Level.ALL);
+        ConsoleHandler handler = new ConsoleHandler();
+
+        handler.setLevel(Level.ALL);
+        handler.setFormatter(new SimpleFormatter());
+        log.addHandler(handler);
+        log.info("Properties of Client Factory not set");
+    }
 }
