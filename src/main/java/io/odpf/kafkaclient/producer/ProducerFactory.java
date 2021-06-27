936d8e78935a2ba76bb2f1d4ec26633a218fc52b
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 20:26:02 +0530

    feat: Override create() in ProducerFactory

--- src/main/java/io/odpf/kafkaclient/producer/ProducerFactory.java
+++ src/main/java/io/odpf/kafkaclient/producer/ProducerFactory.java
@@ -3,4 +3,9 @@ package io.odpf.kafkaclient.producer;
 import io.odpf.kafkaclient.ClientFactory;
 
 public class ProducerFactory extends ClientFactory {
+
+    @Override
+    public Producer create() {
+        return null;
+    }
 }
