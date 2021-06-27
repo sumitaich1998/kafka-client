52f60d07709fdd6dfdb931cfe69ee7dfe7477d1f
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 23:29:01 +0530

    feat: Override printStackTrace() in NullTopicException

--- src/main/java/io/odpf/kafkaclient/consumer/NullTopicException.java
+++ src/main/java/io/odpf/kafkaclient/consumer/NullTopicException.java
@@ -1,4 +1,20 @@
 package io.odpf.kafkaclient.consumer;
 
-public class NullTopicException extends RuntimeException{
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class NullTopicException extends RuntimeException {
+
+    @Override
+    public void printStackTrace() {
+        Logger log = Logger.getLogger("client.logger");
+        log.setLevel(Level.ALL);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        handler.setFormatter(new SimpleFormatter());
+        log.addHandler(handler);
+        log.info("Topic cannot be null");
+    }
 }
