fd2dbfa0680f46348bf3624b06804163d9a118a2
sumitaich1998 <aich.1998@gmail.com>
2021-06-27 20:01:06 +0530

    feat: Implement interact() in Producer

--- src/main/java/io/odpf/kafkaclient/producer/Producer.java
+++ src/main/java/io/odpf/kafkaclient/producer/Producer.java
@@ -2,5 +2,11 @@ package io.odpf.kafkaclient.producer;
 
 import io.odpf.kafkaclient.Client;
 
+import java.io.IOException;
+
 public class Producer extends Client {
+    @Override
+    public void interact() throws IOException {
+
+    }
 }
