a0110866d7eafa466d8d52f10343a5c884cee811
sumitaich1998 <aich.1998@gmail.com>
2021-06-28 18:08:17 +0530

    refactor: Remove unused imports

--- src/main/java/io/odpf/kafkaclient/PropertiesNotSetException.java
+++ src/main/java/io/odpf/kafkaclient/PropertiesNotSetException.java
@@ -1,10 +1,5 @@
 package io.odpf.kafkaclient;
 
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
 public class PropertiesNotSetException extends RuntimeException {
 
     public PropertiesNotSetException(String message) {
