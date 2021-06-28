1cc40d4610d539d6d0ecf47967caa24152108dea
sumitaich1998 <aich.1998@gmail.com>
2021-06-28 17:37:52 +0530

    feat: Add exception constructor

--- src/main/java/io/odpf/kafkaclient/PropertiesNotSetException.java
+++ src/main/java/io/odpf/kafkaclient/PropertiesNotSetException.java
@@ -7,15 +7,7 @@ import java.util.logging.SimpleFormatter;
 
 public class PropertiesNotSetException extends RuntimeException {
 
-    @Override
-    public void printStackTrace() {
-        Logger log = Logger.getLogger("client.logger");
-        log.setLevel(Level.ALL);
-        ConsoleHandler handler = new ConsoleHandler();
-
-        handler.setLevel(Level.ALL);
-        handler.setFormatter(new SimpleFormatter());
-        log.addHandler(handler);
-        log.info("Properties of Client Factory not set");
+    public PropertiesNotSetException(String message) {
+        super(message);
     }
 }
