6acd02efb560d20cdb210f655a4977791ce17fef
sumitaich1998 <aich.1998@gmail.com>
2021-06-28 17:37:47 +0530

    feat: Add exception constructor

--- src/main/java/io/odpf/kafkaclient/consumer/NullTopicException.java
+++ src/main/java/io/odpf/kafkaclient/consumer/NullTopicException.java
@@ -7,15 +7,7 @@ import java.util.logging.SimpleFormatter;
 
 public class NullTopicException extends RuntimeException {
 
-    @Override
-    public void printStackTrace() {
-        Logger log = Logger.getLogger("client.logger");
-        log.setLevel(Level.ALL);
-
-        ConsoleHandler handler = new ConsoleHandler();
-        handler.setLevel(Level.ALL);
-        handler.setFormatter(new SimpleFormatter());
-        log.addHandler(handler);
-        log.info("Topic cannot be null");
+    public NullTopicException(String message) {
+        super(message);
     }
 }
