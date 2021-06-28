233bb745c03ae19aca496a17fa0feada3fef96bb
sumitaich1998 <aich.1998@gmail.com>
2021-06-28 10:07:00 +0530

    feat: Implement readEvent() to read event from Kafka cluster

--- src/main/java/io/odpf/kafkaclient/consumer/Consumer.java
+++ src/main/java/io/odpf/kafkaclient/consumer/Consumer.java
@@ -1,12 +1,17 @@
 package io.odpf.kafkaclient.consumer;
 
 import io.odpf.kafkaclient.Client;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 public class Consumer extends Client {
 
@@ -21,7 +26,21 @@ public class Consumer extends Client {
 
     public ArrayList<String> readEvent(String topic) {
         if (topic == null) throw new NullTopicException();
-        return new ArrayList<>();
+        if (this.topic == null || !(this.topic.equals(topic))) {
+            this.topic = topic;
+            kafkaConsumer.subscribe(Collections.singletonList(topic));
+        }
+        ArrayList<String> recordList = new ArrayList<>();
+        ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(10));
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (ConsumerRecord<String, String> record : records)
+            recordList.add(record.topic() + " " + record.key() + " " + record.value());
+        return recordList;
     }
 
     @Override
