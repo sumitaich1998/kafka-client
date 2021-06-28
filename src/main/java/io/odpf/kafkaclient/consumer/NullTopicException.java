c716f245877f62ff3fdf6403632da56aeb1e63a8
sumitaich1998 <aich.1998@gmail.com>
2021-06-28 18:08:11 +0530

    refactor: Remove unused imports

--- src/main/java/io/odpf/kafkaclient/consumer/NullTopicException.java
+++ src/main/java/io/odpf/kafkaclient/consumer/NullTopicException.java
@@ -1,10 +1,5 @@
 package io.odpf.kafkaclient.consumer;
 
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
 public class NullTopicException extends RuntimeException {
 
     public NullTopicException(String message) {
