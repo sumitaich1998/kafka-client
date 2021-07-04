8ec223cf5840e95144d83f2265d38ece7476d8a9
sumitaich1998 <aich.1998@gmail.com>
2021-07-04 16:01:12 +0530

    fix: Handle invalid application type

--- src/main/java/io/odpf/kafkaclient/Main.java
+++ src/main/java/io/odpf/kafkaclient/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
 
     public static void main(String... args) {
+        try {
+            ApplicationType.valueOf(args[0]);
+        } catch (IllegalArgumentException e) {
+            args[0] = "INVALID";
+        }
         Application app = ProducerConsumerFactory.createApplication(
-                ApplicationType.valueOf(args[0].toUpperCase()), args[1]);
+                ApplicationType.valueOf(args[0]), args[1]);
         app.execute();
     }
 }
