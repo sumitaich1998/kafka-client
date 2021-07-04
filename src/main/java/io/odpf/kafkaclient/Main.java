aa213cbfd7be29e8d05ef7c56fd345fe63982269
sumitaich1998 <aich.1998@gmail.com>
2021-07-04 15:41:29 +0530

    fix: Convert app type to upper case

--- src/main/java/io/odpf/kafkaclient/Main.java
+++ src/main/java/io/odpf/kafkaclient/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String... args) {
         Application app = ProducerConsumerFactory.createApplication(
-                ApplicationType.valueOf(args[0]), args[1]);
+                ApplicationType.valueOf(args[0].toUpperCase()), args[1]);
         app.execute();
     }
 }
