8247fbcd9082a89a65aec12f08e5efb768046360
sumitaich1998 <aich.1998@gmail.com>
2021-07-04 15:39:44 +0530

    docs: Add minimal javadoc

--- src/main/java/io/odpf/kafkaclient/Application.java
+++ src/main/java/io/odpf/kafkaclient/Application.java
@@ -3,9 +3,18 @@ package io.odpf.kafkaclient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The interface Application.
+ */
 public interface Application {
 
- Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    /**
+     * The constant LOGGER.
+     */
+    Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
+    /**
+     * Execute.
+     */
     void execute();
 }
